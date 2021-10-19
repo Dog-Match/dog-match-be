6e28c08c2d25a91bdbd4e9915b461bfb3610483d
Dylan Floyd <floyd800@gmail.com>
2021-10-18 17:50:49 -0700

    add temperaments table and working setup-db

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,24 +17,29 @@ async function run() {
                     user_name VARCHAR(512) NOT NULL, 
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL,
-                    time_to_devote VARCHAR(256) NOT NULL,
-                    activity_level VARCHAR(256) NOT NULL,
-                    time_dog_at_home VARCHAR(256) NOT NULL,
-                    experience_with_dogs VARCHAR(256) NOT NULL,
-                    dwelling_size VARCHAR(256) NOT NULL,
-                    household_size VARCHAR(256) NOT NULL,
-                    children_under_10 VARCHAR(256) NOT NULL,
-                    special_consideration VARCHAR(256) NOT NULL,
-                    main_caretaker VARCHAR(256) NOT NULL,
-                    other_pets VARCHAR(256) NOT NULL,
-                    protective_breed VARCHAR(256) NOT NULL
-                    
+                    time_to_devote INTEGER NOT NULL,
+                    activity_level INTEGER NOT NULL,
+                    time_dog_at_home INTEGER NOT NULL,
+                    experience_with_dogs INTEGER NOT NULL,
+                    dwelling_size INTEGER NOT NULL,
+                    household_size INTEGER NOT NULL,
+                    children_under_10 INTEGER NOT NULL,
+                    special_consideration INTEGER NOT NULL,
+                    main_caretaker INTEGER NOT NULL,
+                    other_pets INTEGER NOT NULL,
+                    protective_breed INTEGER NOT NULL,
+                    state VARCHAR(256) NOT NULL,
+                    zipcode INTEGER NOT NULL
                 );           
                 CREATE TABLE favorites (
                     id VARCHAR(512) NOT NULL,
-                    favorites VARCHAR(512) NOT NULL,
+                    favorite VARCHAR(512) NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
-            );
+                );
+                CREATE TABLE temprs (
+                    id VARCHAR(512) NOT NULL,
+                    tempr_name VARCHAR(512) NOT NULL
+                );
         `);
 
     console.log('create tables complete', getEmoji(), getEmoji(), getEmoji());
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -2,6 +2,7 @@ const client = require('../lib/client');
 // import our seed data:
 const favorites = require('./favorites.js');
 const usersData = require('./users.js');
+const { temprData } = require('./temprData.js');
 const { getEmoji } = require('../lib/emoji.js');
 
 run();
@@ -14,11 +15,11 @@ async function run() {
     const users = await Promise.all(
       usersData.map(user => {
         return client.query(`
-                      INSERT INTO users (email, user_name, hash, time_to_devote, activity_level, time_dog_at_home, experience_with_dogs, dwelling_size, household_size, children_under_10, special_consideration, main_caretaker, other_pets, protective_breed)
-                      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+                      INSERT INTO users (email, user_name, hash, time_to_devote, activity_level, time_dog_at_home, experience_with_dogs, dwelling_size, household_size, children_under_10, special_consideration, main_caretaker, other_pets, protective_breed, state, zipcode)
+                      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
                       RETURNING *;
                   `,
-        [user.email, user.hash, user.time_to_devote, user.activity_level, user.time_dog_at_home, user.experience_with_dogs, user.dwelling_size, user.household_size, user.children_under_10, user.special_consideration, user.main_caretaker, user.other_pets, user.protective_breed]);
+        [user.email, user.user_name, user.hash, user.time_to_devote, user.activity_level, user.time_dog_at_home, user.experience_with_dogs, user.dwelling_size, user.household_size, user.children_under_10, user.special_consideration, user.main_caretaker, user.other_pets, user.protective_breed, user.state, user.zipcode]);
       })
     );
       
@@ -30,7 +31,17 @@ async function run() {
                     INSERT INTO favorites (id, favorite, owner_id)
                     VALUES ($1, $2, $3);
                 `,
-        [1, favorite.name, user.id]);
+        [favorite.id, favorite.favorite, user.id]);
+      })
+    );
+
+    await Promise.all(
+      temprData.map(tempr => {
+        return client.query(`
+                    INSERT INTO temprs (id, tempr_name)
+                    VALUES ($1, $2);
+                `,
+        [tempr.id, tempr.tempr]);
       })
     );
     
