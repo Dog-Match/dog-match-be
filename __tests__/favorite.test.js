b4c7b76a4e72d14a49704b5ebe6a13c7de485301
Estefani <stef45689@gmail.com>
2021-10-20 11:02:57 -0500

    Favorites, Tests and Edit app.js

--- __tests__/favorite.test.js
+++ __tests__/favorite.test.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const { execSync } = require('child_process');
-
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
@@ -22,33 +21,80 @@ describe('app routes', () => {
         });
       
       token = signInData.body.token; // eslint-disable-line
-    }, 50000);
+    }, 500000);
   
     afterAll(done => {
       return client.end(done);
     });
 
+    jest.setTimeout(10000);
+    test('favorites-post', async() => {
+
+      const expectation =  [
+        { 
+          breed_name: 'Mixed',
+          id:expect.any(Number),
+          owner_id: expect.any(Number)
+        }
+      ];
+
+      const data = await fakeRequest(app)
+        .post('/api/favorites')
+        .send({
+          breed_name: 'Mixed',
+          id:3,
+          owner_id:3
+        })
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+
     test('favorites-get', async() => {
 
       const expectation = [
         { 
-          breed_id: expect.any(String),
-          id: expect.any(Number),
-          owner_id:expect.any(Number)
-        },
-        {
-          breed_id: expect.any(String),
+          breed_name: expect.any(String),
           id: expect.any(Number),
           owner_id:expect.any(Number)
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/favorites')
+        .get('/api/favorites')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/);
+        //.expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('favorites-delete', async() => {
+      const expectation = 
+        {         
+          breed_name: 'Mixed',
+          id:1,
+          owner_id:1
+        };
+    
+      const data = await fakeRequest(app)
+        .delete('/api/favorites/Mixed')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+    
+      expect(data.body).toEqual(expectation);
+
+      const Favorites = await fakeRequest(app)
+        .get('/api/favorites')
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
-      expect(data.body).toEqual(expectation);
+      expect(Favorites.body).toEqual(expect.not.arrayContaining([expectation]));
     });
   });
 });
--- data/favorites.js
+++ data/favorites.js
@@ -1,6 +1,13 @@
 module.exports = [
+  { 
+    breed_name: 'Mixed',
+    id:1,
+    owner_id:1
+  },
   {
-    breed_id: 1
+    breed_name: 'Husky',
+    id: 2,
+    owner_id:2 
   }
 ];
   
